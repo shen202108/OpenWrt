263b9a57d4c3b8c866e75900f4168c52206de492
shen202108 <89327134+shen202108@users.noreply.github.com>
2022-07-13 09:05:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改openwrt登陆地址,把第二个的192.168.1.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
