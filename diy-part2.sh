1c3283c0987408e3dc2a6cc9295ff7619d45bb5f
shen202108 <89327134+shen202108@users.noreply.github.com>
2022-07-26 11:34:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 设置登陆密码为6666（密码里面带有 “/” 符号，就把同一个密码再修改一次，一直修改到没带 “/” 符号的就行了）
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$5hySFkd.$pDCbrkMv96w5n0pE4xxL21:19185:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
